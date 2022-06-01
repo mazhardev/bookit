2cefde6d576a825580738dc3690e59f406f5a044
Mazhar <37449942+mazhardev@users.noreply.github.com>
2022-06-01 14:59:22 +0500

    final commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = {
+    env: {
+        DB_LOCAL_URI: '',
+        DB_URI: 'MONGO DB URI',
 
-module.exports = nextConfig
+        STRIPE_API_KEY: '',
+        STRIPE_SECRET_KEY: '',
+
+        STRIPE_WEBHOOK_SECRET: '',
+
+        CLOUDINARY_CLOUD_NAME: '',
+        CLOUDINARY_API_KEY: '',
+        CLOUDINARY_API_SECRET: '',
+
+        SMTP_HOST: "",
+        SMTP_PORT: "",
+        SMTP_USER: "",
+        SMTP_PASSWORD: "",
+        SMTP_FROM_EMAIL: "",
+        SMTP_FROM_NAME: "",
+
+        NEXTAUTH_URL: '',
+    },
+    images: {
+        domains: ['res.cloudinary.com'],
+    },
+}
